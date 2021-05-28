62a7790523669bbd8942969a8a5ec21fc465032a
Endermens <antonveselov14@gmail.com>
2021-05-28 19:00:24 +0300

    Lesson_4

---
+++ Geek_homework/Lesson_4/ex4.py
@@ -0,0 +1,18 @@
+from requests import get, utils
+
+
+response = utils.get_unicode_from_response(get("http://www.cbr.ru/scripts/XML_daily.asp"))
+
+def currency_rates(code):
+    valute = response.split("<Valute ID=")
+    for i in valute:
+        if code.upper() in i:
+            return float(i.replace("/", "").split("<Value>")[1].replace(",", "."))
+
+print(currency_rates("EUR"))
+print(currency_rates("USD"))
+print(currency_rates("GBP"))
+print(currency_rates("Bitcoin"))
+
+if __name__ == "__main__":
+    print("it is not the main executable")
