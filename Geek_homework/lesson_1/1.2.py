46020de6461d15d1f2b78c0789ef68b1478dec64
Endermens Night <75437783+Endermens@users.noreply.github.com>
2021-06-16 19:43:08 +0300

    Delete Geek_homework/lesson_1 directory

--- Geek_homework/lesson_1/1.2.py
+++
@@ -1,8 +0,0 @@
-list = []
-for i in range(1001):
-    if i % 2 != 0:
-        list.append(i)
-print(str(list),"\n")
-
-print(sum(filter(lambda j: sum(map(int, str(j))) % 7 == 0, [i**3 for i in range(1, 1001, 2)])), "- сумма всех чисел кратных 7.\n")
-print(sum(filter(lambda j: sum(map(int, str(j + 17))) % 7 == 0, [i**3 for i in range(1, 1001, 2)])), "- сумма всех чисел после прибаления 17 к числам.")
