a8476c1fac96a6a43ed8b924e182ff1902b0257c
Endermens Night <75437783+Endermens@users.noreply.github.com>
2021-06-17 16:21:12 +0300

    Add files via upload

--- Lesson_10/ex_1.py
+++ Lesson_10/ex_1.py
@@ -11,8 +11,17 @@ class Matrix(object):
     def size(self):
         return (len(self.matrix), len(self.matrix[0]))
 
+    def __add__(self, other):
+        matrix = []
+        for i in range(len(self.matrix)):
+            matrix.append([])
+            for j in range(len(self.matrix[0])):
+                matrix[i].append(self.matrix[i][j] + other.matrix[i][j])
+        return Matrix(matrix)
+
 
 a1 = Matrix([[1, 2, 5], [3, 4, 10], [5, 6, 15]])
-
+a2 = Matrix([[3, 2, 1], [6, 5, 4], [9, 8, 7]])
 print(str(a1))
 print(str(a1.size()))
+print(a1 + a2)
